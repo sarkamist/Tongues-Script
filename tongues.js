9ab767c93aaacc02f8d9546c7be214fec527d1bf
sarkamist <alex_belpo@hotmail.com>
2017-10-12 18:28:12 +0100

    Update tongues.js

--- tongues.js
+++ tongues.js
@@ -1,9 +1,136 @@
 var Tongues = Tongues || (function(){
     'use strict';
     
-    var script = {
-        name: 'Tongues',
-        version: '1.1.2'
+    //---- INFO ----//
+    
+    var script = { name: 'Tongues', version: '2.0.0'},
+        languages = {},
+    
+    //---- PRIVATE FUNCTIONS ----//
+    
+    startup = function(){
+        var handouts = findObjs({                              
+              _type: 'handout',
+        }, {caseInsensitive: true});
+        _.each(handouts, function(handout){
+            loadHandout(handout, false);
+        });
+        
+        log('> ' + script.name + ' (v' + script.version + '): Found ' + Object.keys(languages).length + ' languages <');
+    },
+    
+    loadHandout = function(handout, reload=true){
+        var handoutName = handout.get('name');
+        if (handoutName.startsWith('Tongues: ')){
+            var languageName = handoutName.replace('Tongues: ', '');
+            if (!languages[languageName]){
+                languages[languageName] = {};
+            }
+            languages[languageName].obj = handout;
+            if (languages[languageName].obj){
+                languages[languageName].obj.get('notes', function(notes){
+                    if (notes && notes != 'null'){
+                        var matches = notes.match(/(\[.*?\])/igm);
+                        languages[languageName].words = [];
+                        for (var i = 0; i < matches.length; i++){
+                            if(matches[i] == '[]'){
+                                languages[languageName].words[i] = null;
+                            } else {
+                                matches[i] = matches[i].replace('\[', '').replace('\]', '');
+                                languages[languageName].words[i] = matches[i].split(', ');
+                            }
+                        }
+                        if (reload){
+                            log('> ' + script.name + ' (v' + script.version + '): ' + languageName + ' parsed succesfully! (' + Object.keys(languages).length + ' languages loaded) <');
+                        }
+                        languages[languageName].obj.get('gmnotes', function(gmnotes){
+                            if (gmnotes && gmnotes != 'null'){
+                                languages[languageName].speakers = gmnotes.split(', ');
+                            } 
+                        });
+                    }
+                });
+            } else {
+                delete languages[languageName];
+                log('> ' + script.name + ' (v' + script.version + '): ' + languageName + ' has a wrong format and cannot be parsed! (' + Object.keys(languages).length + ' languages loaded) <');
+            }
+        } else {
+            _.each(Object.keys(languages), function(key){
+                if(languages[key].obj.get('id') === handout.get('id')){
+                    delete languages[key];
+                    log('> ' + script.name + ' (v' + script.version + '): ' + key + ' removed successfully! (' + Object.keys(languages).length + ' languages loaded) <');
+                }
+            });
+        }
+        
+        
+        return;
+    },
+    deleteHandout = function(handout){
+        var handoutName = handout.get('name');
+        if (handoutName.startsWith('Tongues: ')){
+            var languageName = handoutName.replace('Tongues: ', '');
+            delete languages[languageName];
+            log('> ' + script.name + ' (v' + script.version + '): ' + languageName + ' removed successfully! (' + Object.keys(languages).length + ' languages loaded) <');
+        }
+    },
+    
+    commandCreate = function(msg, command){
+        var newLanguage = createObj('handout', {
+            name: 'Tongues: ' + command[3]
+        });
+        var text = '';
+        for(var i = 0; i < 14; i++){
+            text += command[3] + ' ' + (i+1) + '-letter words: [] <br>'
+        }
+        newLanguage.set({
+            notes: text
+        });
+        sendChat('Tongues', '/w ' + msg.who + ' ' + command[3] + ' language template succesfully created!', null, {noarchive:true});
+    },
+    commandSpeak = function(msg, command){
+        if (!msg.selected){
+            sendChat('Tongues', '/w ' + msg.who + ' You must select a character token!', null, {noarchive:true});
+        } else if (msg.selected.length > 1){
+            sendChat('Tongues', '/w ' + msg.who + ' You must select only one character token!', null, {noarchive:true});
+        } else {
+            var token = getObj('graphic', msg.selected[0]._id);
+            var speakerId = token.get('represents');
+            var languageName = command[2];
+            var text = command[3];
+            if (languages[languageName] && languages[languageName].obj){
+                if (languages[languageName].speakers){
+                    if (isLanguageSpeaker(speakerId, languages[languageName].speakers)){
+                        translate(speakerId, languageName, text);
+                    } else {
+                        sendChat('Tongues', '/w ' + msg.who + ' That character cannot speak ' + languageName + '!', null, {noarchive:true});
+                    }
+                } else {
+                    sendChat('Tongues', '/w ' + msg.who + ' There are no speakers of ' + languageName + '!', null, {noarchive:true});
+                }
+            } else {
+                sendChat('Tongues', '/w ' + msg.who + ' ' + languageName + ' does not exist as a Tongues valid language!', null, {noarchive:true});
+            }
+        }
+    },
+    
+    translate = function(speakerId, languageName, text){
+        var translatedText = text.replace(/([a-z]+)/igm, function(match){
+            var n = 1;
+            while (!languages[languageName].words[match.length - n] && match.length - n >= 0){
+                n++;
+            }
+            if (match.length - n < 0){
+                return match.replace(/[a-z]/ig, '?');
+            } else {
+                return matchCase(languages[languageName].words[match.length - n][Math.floor(Math.random() * languages[languageName].words[match.length - n].length)], match);
+            }
+        });
+        sendChat('character|' + speakerId, '[' + languageName + '] ' + translatedText);
+        sendChat('Tongues', '/w GM [' + languageName + '] ' + text);
+        _.each(languages[languageName].speakers, function(speakerName){
+            sendChat('Tongues', '/w ' + speakerName + ' [' + languageName + '] ' + text);
+        });
     },
     
     isLanguageSpeaker = function(speakerId, speakers) {
@@ -37,93 +164,39 @@ var Tongues = Tongues || (function(){
         return result;
     },
     
-    translate = function(speakerId, language, text, languageSpeakers){
-        language.get('notes', function(notes){
-            var matches = notes.match(/(\[.*?\])/igm);
-            var words = [];
-            for (var i = 0; i < matches.length; i++){
-                matches[i] = matches[i].replace('\[', '').replace('\]', '');
-                words[i] = matches[i].split(', ');
-            }
-            var translatedText = text.replace(/([a-z]+)/igm, function (match){
-                var n = 1;
-                while (!words[match.length - n] && match.length - n >= 0){
-                    n++;
-                }
-                if (match.length - n < 0){
-                    return match.replace(/./, '?');
-                } else {
-                    return matchCase(words[match.length - n][Math.floor(Math.random() * words[match.length - n].length)], match);
-                }
-            });
-            sendChat('character|' + speakerId, '[' + language.get('name') + '] ' + translatedText);
-            sendChat('Tongues', '/w GM ' + text);
-            if (languageSpeakers && languageSpeakers != 'null'){
-                _.each(languageSpeakers, function(speakerName){
-                    sendChat('Tongues', '/w ' + speakerName + ' [' + language.get('name') + '] ' + text);
-                });
-            }
-        });
-    },
+    //----- INPUT HANDLER -----//
     
     handleInput = function(msg){
         if (msg.type == 'api' && msg.content.startsWith('!tongues ')){
             var regex = /(\![^\ ]+) ([^\ ]+) (.+)/igm;
             var command = regex.exec(msg.content);
-            if(command[2] == '--create'){
-                var newLanguage = createObj('handout', {
-                    name: command[3]
-                });
-                var text = '';
-                for(var i = 0; i < 14; i++){
-                    text += command[3] + ' ' + (i+1) + '-letter words: [] <br>'
-                }
-                newLanguage.set({
-                        notes: text
-                });
-                sendChat('Tongues', '/w ' + msg.who + ' ' + command[3] + ' language template succesfully created!', null, {noarchive:true})
-            } else if (command[2].indexOf("--") !== -1) {
-                sendChat('Tongues', '/w ' + msg.who + ' No valid command!', null, {noarchive:true})
-            } else {
-                if (!msg.selected){
-                    sendChat('Tongues', '/w ' + msg.who + ' You must select a character token!', null, {noarchive:true})
-                } else if (msg.selected.length > 1){
-                    sendChat('Tongues', '/w ' + msg.who + ' You must select only one character token!', null, {noarchive:true})
-                } else {
-                    var token = getObj('graphic', msg.selected[0]._id);
-                    var speakerId = token.get('represents');
-                    var text = command[3];
-                    var language = findObjs({                              
-                      _type: 'handout',
-                      name: command[2],
-                    }, {caseInsensitive: true})[0];
-                    if (language){
-                        language.get('gmnotes', function(gmnotes){
-                            if (gmnotes && gmnotes != 'null'){
-                                var speakers = gmnotes.split(', ');
-                                if (isLanguageSpeaker(speakerId, speakers)){
-                                    translate(speakerId, language, text, speakers);
-                                } else {
-                                    sendChat('Tongues', '/w ' + msg.who + ' That character cannot speak ' + language.get('name') + '!', null, {noarchive:true});
-                                }
-                            } else {
-                                sendChat('Tongues', '/w ' + msg.who + ' There are no speakers of ' + language.get('name') + '!', null, {noarchive:true});
-                            }
-                        });
+            if(command[2].startsWith("--")){
+                if(playerIsGM(msg.playerid)){
+                    if(command[2] == '--create'){
+                        commandCreate(msg, command);
                     } else {
-                        sendChat('Tongues', '/w ' + msg.who + ' ' + command[2] + ' does not exist as a Tongues valid language!', null, {noarchive:true});
+                        sendChat('Tongues', '/w ' + msg.who + ' No valid command!', null, {noarchive:true})
                     }
+                } else {
+                    sendChat('Tongues', '/w ' + msg.who + ' Only the GM can access Tongues configuration commands!', null, {noarchive:true})
                 }
+            } else {
+                commandSpeak(msg, command);
             }
         }
     },
     
-    registerEventHandlers = function() {
+    //---- PUBLIC FUNCTIONS ----//
+    
+    registerEventHandlers = function(){
 		on('chat:message', handleInput);
+		on('change:handout', loadHandout);
+		on('destroy:handout', deleteHandout);
 	},
     
     checkInstall = function(){
-        log('-=> ' + script.name + ' (v' + script.version + ') <=-');
+        log('> ' + script.name + ' (v' + script.version + ') is installed and running <');
+        startup();
     };
     
     return {
